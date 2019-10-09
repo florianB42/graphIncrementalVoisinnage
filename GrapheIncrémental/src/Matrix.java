e3f0a39c74a65685fb4b481fdfbdaa565beafdc4
Florian <Florian@LAPTOP-B8KN8PJ2>
2019-10-09 18:16:52 +0200

    test

--- GrapheIncrémental/src/Matrix.java
+++ GrapheIncrémental/src/Matrix.java
@@ -3,6 +3,9 @@ import java.util.HashMap;
 public class Matrix {
 	private HashMap<String, Boolean> matrix = new HashMap<String, Boolean>();
 	private int sizeMatrix = 0;
-	
-	
+
+	public Boolean getMatrice(int un, int deux) {
+		return un == deux;
+	}
+
 }
