e6697b37a3127f5a3f044cb3beafe7ef26ccbfea
Florian <Florian@LAPTOP-B8KN8PJ2>
2019-10-19 13:01:03 +0200

    matrix hash

--- GrapheIncrémental/src/Matrix.java
+++ GrapheIncrémental/src/Matrix.java
@@ -3,66 +3,97 @@ import java.util.HashMap;
 public class Matrix {
 	// public HashMap<PosMatrix, Boolean> matrix = new HashMap<PosMatrix,
 	// Boolean>();
-	public HashMap<Integer, HashMap<Integer, Boolean>> matrix = new HashMap<Integer, HashMap<Integer, Boolean>>();
+	// public HashMap<Integer, HashMap<Integer, Boolean>> matrix = new
+	// HashMap<Integer, HashMap<Integer, Boolean>>();
 	private int sizeMatrix = 0;
+	public HashMap<Integer, Boolean> matrix = new HashMap<Integer, Boolean>();
 
-	public Boolean getEdges(int i, int j) throws Exception {
-		if (i < j)
-			if (matrix.containsKey(i))
-				if (matrix.get(i).containsKey(j))
-					return true;
-				else
-					return false;
-			else
-				return false;
-		else if (i > j)
-			if (matrix.get(j) != null && matrix.get(j).get(i))
-				return true;
-			else
-				return false;
-		else
+	public Boolean getEdges(Integer i, Integer j) throws Exception {
+//		if (i < j)
+//			if (matrix.containsKey(i))
+//				if (matrix.get(i).containsKey(j))
+//					return true;
+//				else
+//					return false;
+//			else
+//				return false;
+//		else if (i > j)
+//			if (matrix.containsKey(j))
+//				if (matrix.get(j).containsKey(i))
+//					return true;
+//				else
+//					return false;
+//			else
+//				return false;
+//		else
+//			throw new Exception("Matrix_IllegalEdgeRead");
+		String stringToHash;
+		int hashcode;
+		if (i < j) {
+			stringToHash = (i.toString() + j.toString());
+			hashcode = stringToHash.hashCode();
+		} else if (i > j) {
+			stringToHash = (i.toString() + j.toString());
+			hashcode = stringToHash.hashCode();
+		} else
 			throw new Exception("Matrix_IllegalEdgeRead");
-		
+		return matrix.containsKey(hashcode) ? matrix.get(hashcode) : false;
+
 	}
 
-	public void createEdges(int i, int j) throws Exception{
-		if (i < j)
-			if (matrix.containsKey(i))
-				matrix.get(i).put(j, true);
-			else {
-				matrix.put(i, new HashMap<Integer,Boolean>());
-				matrix.get(i).put(j, true);
-			}
-		else if (i > j)
-			if (matrix.containsKey(i))
-				matrix.get(j).put(i, true);
-			else {
-				matrix.put(j, new HashMap<Integer,Boolean>());
-				matrix.get(j).put(i, true);
-				}
-		else 
+	public void createEdges(Integer i, Integer j) throws Exception {
+//		String test = (i.toString() + j.toString());
+//		int tet = test.hashCode();
+//		System.out.println(tet);
+//		if (i < j)
+//			if (matrix.containsKey(i))
+//				matrix.get(i).put(j, true);
+//			else {
+//				matrix.put(i, new HashMap<Integer, Boolean>());
+//				matrix.get(i).put(j, true);
+//			}
+//		else if (i > j)
+//			if (matrix.containsKey(i))
+//				matrix.get(j).put(i, true);
+//			else {
+//				matrix.put(j, new HashMap<Integer, Boolean>());
+//				matrix.get(j).put(i, true);
+//			}
+//		else
+//			throw new Exception("Matrix_IllegalEdgeCreate");
+		
+		String stringToHash;
+		int hashcode;
+		if (i < j) {
+			stringToHash = (i.toString() + j.toString());
+			hashcode = stringToHash.hashCode();
+		} else if (i > j) {
+			stringToHash = (i.toString() + j.toString());
+			hashcode = stringToHash.hashCode();
+		} else
 			throw new Exception("Matrix_IllegalEdgeCreate");
+		matrix.put(hashcode, true);
 	}
-	
-	public void deleteEdges(int i, int j) throws Exception{
-		if (i < j)
+
+	public void deleteEdges(int i, int j) throws Exception {
+		/*if (i < j)
 			matrix.get(i).remove(j);
 
 		else if (i > j)
 			matrix.get(j).remove(i);
-		
-		else 
-			throw new Exception("Matrix_IllegalEdgeDelete");
+
+		else
+			throw new Exception("Matrix_IllegalEdgeDelete");*/
 	}
 
 	public static void main(String[] args) throws Exception {
 		Matrix test = new Matrix();
 		test.createEdges(0, 1);
 		test.createEdges(2, 1);
-		test.createEdges(0, 2);
-		test.createEdges(0, 1);
-		test.deleteEdges(0, 1);
-		
+		test.createEdges(1, 2);
+		test.createEdges(1, 0);
+		//test.deleteEdges(0, 1);
+
 		System.out.println(test.getEdges(0, 1));
 		System.out.println(test.getEdges(2, 0));
 		System.out.println(test.getEdges(2, 1));
@@ -75,40 +106,17 @@ public class Matrix {
 		 */
 	}
 }
-
-class PosMatrix implements Comparable<PosMatrix> {
-	int i;
-	int j;
-
-	public PosMatrix(int i, int j) {
-		this.i = i;
-		this.j = j;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		System.out.println("eed");
-		if (obj == null || obj.getClass() != getClass())
-			return false;
-		else {
-		}
-		PosMatrix nObj = (PosMatrix) obj;
-		if (nObj.i == i && nObj.j == j)
-			return true;
-		else
-			return false;
-	}
-
-	@Override
-	public int compareTo(PosMatrix obj) {
-		System.out.println("eed");
-		if (obj == null)
-			return 1;
-		else {
-			if (obj.i == i && obj.j == j)
-				return 0;
-			else
-				return 1;
-		}
-	}
-}
+/*
+ * class PosMatrix implements Comparable<PosMatrix> { int i; int j;
+ * 
+ * public PosMatrix(int i, int j) { this.i = i; this.j = j; }
+ * 
+ * @Override public boolean equals(Object obj) { System.out.println("eed"); if
+ * (obj == null || obj.getClass() != getClass()) return false; else { }
+ * PosMatrix nObj = (PosMatrix) obj; if (nObj.i == i && nObj.j == j) return
+ * true; else return false; }
+ * 
+ * @Override public int compareTo(PosMatrix obj) { System.out.println("eed"); if
+ * (obj == null) return 1; else { if (obj.i == i && obj.j == j) return 0; else
+ * return 1; } } }
+ */
