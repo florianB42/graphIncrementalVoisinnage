6432b72f1009723de6bc028fbf4a7ce03f7cf2c2
ahlam <ahlam@LAPTOP-7BC137KH>
2019-11-28 18:30:39 +0100

    2D Array to store distances in constructRNG() function (OPerationsGraph Class)

--- GrapheIncrémental/src/OperationsGraph.java
+++ GrapheIncrémental/src/OperationsGraph.java
@@ -3,15 +3,14 @@ public class OperationsGraph {
 	
 	//////////////////////////////Methods///////////////////////////////////
 	public void constructRNG(Graph graph) {
-//		int compteur = 0;
-//		double[] arrayDist = new double[compteur];
+		double[][] arrayDist = new double[graph.getNbVertices()][graph.getNbVertices()];
 		for(int vertexIndex1 = 0; vertexIndex1 < graph.getNbVertices(); vertexIndex1++) {
 			for(int vertexIndex2 = vertexIndex1 + 1; vertexIndex2 < graph.getNbVertices(); vertexIndex2++) {
-				calculDist(graph.getVertexFromList(vertexIndex1), graph.getVertexFromList(vertexIndex2));
-				//compteur += 1;
+				arrayDist[vertexIndex1][vertexIndex2] = calculDist(graph.getVertexFromList(vertexIndex1),
+																   graph.getVertexFromList(vertexIndex2));
 			}
 		}
-		//TODO : store results in 2D array ...
+		//TODO : choose the edge to draw ...
 	}
 	public Vertex findNearest(Vertex vertex, Graph graph) {
 		return vertex ;
@@ -26,7 +25,7 @@ public class OperationsGraph {
 	public double calculDist(Vertex vertex1, Vertex vertex2) {
 		double distance, somme = 0;
 		for(int i = 0; i < vertex1.getSizeList(); i++) {
-			somme = Math.pow((vertex1.getData(i) - vertex2.getData(i)), 2);
+			somme += Math.pow((vertex1.getData(i) - vertex2.getData(i)), 2);
 		}
 		distance = Math.sqrt(somme);
 		return distance;
@@ -36,6 +35,6 @@ public class OperationsGraph {
 		
 	}
 	void mergeSubgraph(Graph mainGraph, Graph subGraph) {
-		
+		//TODO
 	}
 }
