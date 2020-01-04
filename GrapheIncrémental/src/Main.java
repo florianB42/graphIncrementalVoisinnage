302ee2e552d4650149642a1225dd3944e6e7065b
Florian <Florian@LAPTOP-B8KN8PJ2>
2020-01-04 20:06:25 +0100

    fichier d'enterer en paramettre

--- GrapheIncrémental/src/Main.java
+++ GrapheIncrémental/src/Main.java
@@ -14,9 +14,13 @@ public class Main {
 		Parser parserMM;
 		Parser parser;
 		try {
-			parserMM = new Parser("D:\\Polytech\\4A-S7\\projet\\wine.Data", ",");
-			parser = new Parser("D:\\Polytech\\4A-S7\\projet\\wine.Data", ",");
-
+			//parserMM = new Parser("D:\\Polytech\\4A-S7\\projet\\wine.Data", ",");
+			//parser = new Parser("D:\\Polytech\\4A-S7\\projet\\wine.Data", ",");
+			//parserMM = new Parser("D:\\Polytech\\4A-S7\\projet\\winequality-white.csv", ";");
+			//parser = new Parser("D:\\Polytech\\4A-S7\\projet\\winequality-white.csv", ";");
+			parserMM = new Parser(args[0], args[1]);
+			parser = new Parser(args[0], args[1]);
+			
 			opGraph.readMaxMinValueGraph(parserMM, graph);
 			
 			vertex = opGraph.readNewVertex(parser, idVertex, graph);
@@ -25,15 +29,15 @@ public class Main {
 			
 			while ((vertex = opGraph.readNewVertex(parser, idVertex, graph)) != null) {
 		System.out.println(vertex);
-				Graph subGraph = opGraph.extractSubgraph(graph, opGraph.findNearest(vertex, graph));
-				subGraph.addVertex(vertex);
-		System.out.println(subGraph.getListVertex());
-				opGraph.constructRNG(subGraph);
+				//Graph subGraph = opGraph.extractSubgraph(graph, opGraph.findNearest(vertex, graph));
+				//subGraph.addVertex(vertex);
+		//System.out.println(subGraph.getListVertex());
+				//opGraph.constructRNG(subGraph);
 				graph.addVertex(vertex);
-				opGraph.mergeSubgraph(graph, subGraph);
+				//opGraph.mergeSubgraph(graph, subGraph);
 				++idVertex;
 			}
-			//opGraph.constructRNG(graph);
+			opGraph.constructRNG(graph);
 
 			opGraph.writeTulipFile(graph);
 		} catch (FileNotFoundException e) {
--- GrapheIncrémental/src/graphManagement/OperationsGraph.java
+++ GrapheIncrémental/src/graphManagement/OperationsGraph.java
@@ -177,7 +177,7 @@ public class OperationsGraph {
 		while (parser.readLigne() != null) {
 			try {
 				Integer numData = 0;
-				parser.parserNext();
+				//parser.parserNext();
 				do {
 					graph.setVertexDataMax(numData, parser.parserFloat());
 					graph.setVertexDataMin(numData, parser.parserFloat());
@@ -210,8 +210,8 @@ public class OperationsGraph {
 				 * parser.parserNext(); } vertex.setCategory(parser.parserString()); return
 				 * vertex;
 				 */
-			vertex.setCategory(parser.parserString());
-			parser.parserNext();
+			//vertex.setCategory(parser.parserString());
+			//parser.parserNext();
 			do {
 				try {
 					newValue = (parser.parserFloat() - graph.getVertexDataMin(numData))
